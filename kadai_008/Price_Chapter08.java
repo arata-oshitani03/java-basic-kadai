db2063960a834f342fe630de385c14206ec7d53a
arata-oshitani03 <159011416+arata-oshitani03@users.noreply.github.com>
2024-03-22 15:04:49 +0900

    課題提出
    switch文での課題

---
+++ kadai_008/Price_Chapter08.java
@@ -0,0 +1,29 @@
+package kadai_008;
+
+public class Price_Chapter08 {
+
+	public static void main(String[] args) {
+		
+		//変数を宣言・初期化
+       int userAge = 30;
+	   int serviceCost = 3000;
+	   
+	   //年代に応じて料金を分布
+	   switch(userAge) {
+       case 10  -> serviceCost = 1000;
+       case 20  -> serviceCost = 2000;
+       case 30  -> serviceCost = 3000;
+       case 40  -> serviceCost = 4000;
+       case 50  -> serviceCost = 5000;
+       case 60  -> serviceCost = 6000;
+       case 70  -> serviceCost = 7000;
+       case 80  -> serviceCost = 8000;
+       default  -> serviceCost = 500;
+   };
+   
+   // 賞品名を表示
+      System.out.println(userAge + "代の料金は" + serviceCost + "円");
+
+	}
+
+}
